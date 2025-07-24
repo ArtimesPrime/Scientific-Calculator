66855a2e553002e2a3f211024c8097878c5237c5
ArtimesPrime <eb20190405@gmail.com>
2025-07-25 10:46:48 +1200

    Add files via upload
    Added numpad, reorganized button and started adding labels to each number. Currently broken as the main mass of buttons doesn't have all the labels made

--- GUI - Test.py
+++ GUI - Test.py
@@ -16,6 +16,26 @@ class calculator:
         self.container = Frame(self.root)
         self.container.grid(row=0, column=0, sticky=NSEW)
 
+        self.Labels = {
+            "10": "",
+            "50": "1",
+            "51": "2",
+            "52": "3",
+            "53": "X",
+            "54": "÷",
+            "60": "4",
+            "61": "5",
+            "62": "6",
+            "63": "+",
+            "64": "-",
+            "70": "7",
+            "71": "8",
+            "72": "9",
+            "73": "DEL",
+            "74": "EXE",
+            "80": "0",
+            "81": "."}
+
 
         self.frames = {}
         self.frames["Main Calculator"] = self.Main_Calculator()
@@ -49,30 +69,43 @@ class calculator:
    
     def Main_Calculator(self):
         frame = Frame(self.container)
-        frame.rowconfigure([0,1,2,3,4,5,6,7,8], weight=1, minsize=30)
-        frame.columnconfigure([0,1,2,3,], weight=1, minsize=60)
+        frame.rowconfigure([0,1,2,3,4,5,6,7,8], weight=1, minsize=40)
+        frame.columnconfigure([0,1,2,3,4], weight=1, minsize=60)
 
         self.Equationbox = Text(frame, width=40, height=2)
         self.Equationbox.grid(row=0, columnspan=8,pady=5, padx=5)
 
 
-        for i in range(1, 6):
-            for j in range(4):
-                self.CenterButtons = Button(frame, text="center", width=5, command=lambda i=i, j=j: self.Operators(i, j))
+        for i in range(1, 5):
+            for j in range(0,5):
+                Name = str(i) + str(j)
+                self.CenterButtons = Button(frame, text=self.Labels[Name], width=5, command=lambda i=i, j=j: self.Operators(i, j))
                 self.CenterButtons.grid(row=i, column=j, pady=3)
         
 
 
-        for i in range(6,9):
-            for j in range(2,4):
-                self.RightButtons = Button(frame, text="Right", width=5, command=lambda i=i, j=j: self.Operators(i, j))
+        for i in range(5,8):
+            for j in range(3,5):
+                Name = str(i) + str(j)
+                self.RightButtons = Button(frame, text=self.Labels[Name], width=5, command=lambda i=i, j=j: self.Operators(i, j))
                 self.RightButtons.grid(row=i, column=j, pady=3)
+
+        
+        for i in range(5,8):
+            for j in range(0,3):
+                Name = str(i) + str(j)
+                self.NumButtons = Button(frame, text=self.Labels[Name], width=5, height=2, command=lambda i=i, j=j: self.Operators(i, j))
+                self.NumButtons.grid(row=i, column=j, pady=3)
+        
+        for i in range(8,9):
+            for j in range(0,2):
+                Name = str(i) + str(j)
+                self.PlaceButtons = Button(frame, text=self.Labels[Name], width=5, command=lambda i=i, j=j: self.Operators(i, j))
+                self.PlaceButtons.grid(row=i, column=j, pady=3)
 
         frame.grid(row=0, column=0)
         return frame
 
-    def Numbers(self):
-        Frame(self.container)
 
 
     def Quadratic_Solver(self):
